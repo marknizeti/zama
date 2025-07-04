66f4d67697f1e5eebedba530cad25366d706e059
marknizeti <mark.nizeti@gmail.com>
2025-07-04 10:14:10 +0200

    Šesti commit: Ažuriran hardhat.config.js za FHEVM podršku

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    fhevm: {
+      url: "http://localhost:8545",
+      chainId: 9000,
+    },
+  },
+};
