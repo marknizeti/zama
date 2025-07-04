73409fc3b62f1e1e7570a7997efa27b268d4085b
marknizeti <mark.nizeti@gmail.com>
2025-07-04 10:23:45 +0200

    Sedmi commit: Ažuriran hardhat.config.js za podršku Solidity 0.8.28

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,26 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: {
-    version: "0.8.24",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
-    },
+      {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   networks: {
     hardhat: {
